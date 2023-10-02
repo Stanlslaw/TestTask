d512b70a8cb5c36d3b5009a188a8674499dbd767
Stanislaw Skalkovich <stas.skalkovich@gmail.com>
2023-10-03 00:40:14 +0300

    done

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class OrderService:IOrderService
+{
+    #region Fields
+    private readonly ApplicationDbContext _appDbContext;
+    #endregion
+    
+    #region Constructors
+    public OrderService(ApplicationDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+    #endregion
+
+    #region Methods
+
+    ///<returns>
+    /// Returns the order with the highest total price (Price*Quantity) (or null)
+    /// </returns>
+    public async Task<Order?> GetOrder()
+    {
+        return await _appDbContext.Orders
+            .OrderByDescending(orders => orders.Price * orders.Quantity)
+            .FirstOrDefaultAsync();
+    }
+    ///<returns>
+    /// Returns the list of orders with where quantity greater then 10 (or empty list)
+    /// </returns>
+    public async Task<List<Order>> GetOrders()
+    {
+        return await _appDbContext.Orders
+            .Where(orders => orders.Quantity > 10)
+            .ToListAsync();
+    }
+
+    #endregion
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class UserService:IUserService
+{
+    #region Fields
+    private readonly ApplicationDbContext _appDbContext;
+    #endregion
+    
+    #region Constructors
+    public UserService(ApplicationDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+    #endregion
+
+    #region Methods
+    ///<returns>
+    /// Returns the user with the highest orders count (or null)
+    /// </returns>
+    public async Task<User?> GetUser()
+    {
+        /*
+         * Хочется сделать замечание к данной реализации,
+         * в нашем случае требуется вернуть одного пользователя,где данная реализация верна,
+         * однако может возникнуть ситуация, когда будет несколько пользователей с одним количеством заказов,
+         * и в данной реализации мы получим только одного пользователя, что может привести к получению неполного результата
+         * для определенной цели.
+         */
+
+        return await _appDbContext.Users
+            .Where(users=>users.Status!=UserStatus.Inactive)
+            //.Include(users=>users.Orders)
+            /*
+             * Спорная ситуация возникла, по заданию просят вернуть пользователя,
+             * этим я и оперировал, однако было бы полезно вернуть также и список его заказов,
+             * но для этого придется создавать костыли, чтобы избежать циклической зависимости (P.S. познаю новое) :),
+             * поэтому для получения списка заказов лучше создать отдельный метод 
+             */
+            .OrderByDescending(users => users.Orders.Count)
+            .FirstOrDefaultAsync();
+
+       
+    }
+    ///<returns>
+    /// Returns the list of inactive users (or empty list)
+    /// </returns>
+    public async Task<List<User>> GetUsers()
+    {
+        return await _appDbContext.Users
+            .Where(users => users.Status == UserStatus.Inactive).ToListAsync();
+    }
+    #endregion
+  
+}
